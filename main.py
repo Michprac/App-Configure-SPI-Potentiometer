dbdb6dbd9ff92adf0f377e01dcebaa03820ec446
Michprac <michal.poczta135@gmail.com>
2023-04-10 13:52:54 +0200

    Setiing icon added

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import tkinter as tk
 
 window = tk.Tk()
-window.config(width='200', height='300')
+height = 500
+width = 500
+d_x = 500
+d_y = 100
+window.title('Configuration of the SPI Potentiometer')
+window.geometry(f"{height}x{width}+{d_x}+{d_y}")
+window.minsize(300, 300)
+window.maxsize(700, 700)
+window.resizable(True, True)
+
+photo = tk.PhotoImage(file='setting.png')
+window.iconphoto(True, photo)
 
 window.mainloop()
