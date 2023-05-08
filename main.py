191b609d68545d0bac247bc41734abf4d5025ea0
Michprac <michal.poczta135@gmail.com>
2023-05-08 20:38:59 +0200

    Added graphs

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter.messagebox import showerror
 
 from serial.tools import list_ports
 import serial
 
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+
 
 ports = list_ports.comports()
 serial_object = serial.Serial()
@@ -31,16 +34,23 @@ def choose_pt():
 
 
 window = tk.Tk()
-height = 750
-width = 850
+
+
+
+
+height = 980
+width = 859
 d_x = 500
-d_y = 20
+d_y = 0
 Radio_pt = tk.IntVar()  # variable for the radiobutton choosing
+
+
 setvalue1 = tk.StringVar()  # actual value for the potentiometer 1
 scale1 = tk.IntVar()
 
 setvalue2 = tk.StringVar()  # actual value for the potentiometer 2
 scale2 = tk.IntVar()
+
 
 pt1_active = tk.IntVar()
 pt1_active.set(0)
@@ -50,6 +60,49 @@ ValuesForPotentiometer = {'Choose value': 'Choose value',
                           '30 000': 30000,
                           '40 000': 40000,
                           '50 000': 50000}
+
+previous_values_pt_1 = [0]*10
+actual_value_number_1 = list(range(1,11))
+previous_values_pt_2 = [0]*10
+actual_value_number_2 = list(range(1,11))
+
+#Creating a plot of previous values
+def create_plot1():
+
+    fig = plt.figure(figsize=(3,3), dpi=90)
+    plot1 = fig.add_subplot(1,1,1)
+    plot1.set_ylabel('Value of the potentiometer 1')
+    plot1.set_xlabel('Previous value number')
+    plot1.set_title('Graph for potentiometer 1')
+    plot1.plot(actual_value_number_1, previous_values_pt_1, marker='o')
+
+    plot_on_window = FigureCanvasTkAgg(fig, window)
+    plot_on_window.draw()
+    plot_on_window.get_tk_widget().grid(row=5, column=0, columnspan=8, sticky='NWES', pady=5, ipady=50, padx=20)
+
+
+def create_plot2():
+
+    global fig, axis
+
+    fig = plt.figure(figsize=(3,3), dpi=90)
+    plot1 = fig.add_subplot(1,1,1)
+    plot1.set_ylabel('Value of the potentiometer 2')
+    plot1.set_xlabel('Previous value number')
+    plot1.set_title('Graph for potentiometer 2')
+
+    plot1.plot(actual_value_number_2, previous_values_pt_2, marker='o')
+
+    plot_on_window = FigureCanvasTkAgg(fig, window)
+    plot_on_window.draw()
+    plot_on_window.get_tk_widget().grid(row=6, column=0, columnspan=8, sticky='NWES', pady=5, ipady=50, padx=20)
+
+
+
+
+
+
+
 
 
 def unlock_widgets1():
@@ -66,7 +119,7 @@ def unlock_widgets1():
         label1.config(state="disabled")
         box1.config(state="disabled")
         entry1.config(state="disabled")
-        scale1_widg.config(state='disabled')
+        scale1_widg.config(state=tk.DISABLED)
         send_btn1.config(state="disabled")
         actual_val_1.config(state="disabled")
         val_1.config(state="disabled")
@@ -86,7 +139,7 @@ def unlock_widgets2():
         box2.config(state="disabled")
         label2.config(state="disabled")
         entry2.config(state="disabled")
-        scale2_widg.config(state='disabled')
+        scale2_widg.config(state=tk.DISABLED)
         send_btn2.config(state="disabled")
         actual_val_2.config(state="disabled")
         val_2.config(state="disabled")
@@ -163,9 +216,9 @@ def set_values_entry_2(event):
     val_2['text'] = str(newval) + '[Ω]'
 
 def set_values_scale_1(event):
-
+    if scale1_widg["state"] == "disabled":
+        return
     newval = int(round(scale1.get() / 100 * max(list(ValuesForPotentiometer.values())[1:])))
-    print(newval)
     setvalue1.set(newval)
 
     box1.set(list(ValuesForPotentiometer.keys())[0])
@@ -176,11 +229,11 @@ def set_values_scale_1(event):
     val_1['text'] = str(newval) + '[Ω]'
 
 
-
 def set_values_scale_2(event):
+    if scale2_widg["state"] == "disabled":
+        return
 
     newval = int(round(scale2.get() / 100 * max(list(ValuesForPotentiometer.values())[1:])))
-    print(newval)
     setvalue2.set(newval)
 
     box2.set(list(ValuesForPotentiometer.keys())[0])
@@ -192,13 +245,60 @@ def set_values_scale_2(event):
 
 
 
+def send_value_1():
+    try:
+        int(setvalue1.get())
+    except ValueError:
+        showerror(title="Error", message="Write a NUMBER, not string")
+        return
 
+    global previous_values_pt_1
+
+
+
+    # configure_byte = 0b00010001
+    # value_of_pt_hex_1 = int(setvalue1.get()) * 255 // max(list(ValuesForPotentiometer.values())[1:])
+    # text_to_send = str(configure_byte)[2:] + str(bin(value_of_pt_1))
+    # serial_object.write(text_to_send)
+
+
+    previous_values_pt_1 = previous_values_pt_1[1:]
+    previous_values_pt_1.append(setvalue1.get())
+
+    create_plot1()
+
+
+
+def send_value_2(list1):
+
+    try:
+        int(setvalue1.get())
+    except ValueError:
+        showerror(title="Error", message="Write a NUMBER, not string")
+        return
+
+    global previous_values_pt_2
+
+
+    # configure_byte = 0b00010010
+    # value_of_pt_1 = int(setvalue2.get()) * 255 // max(list(ValuesForPotentiometer.values())[1:])
+    # text_to_send = str(configure_byte)[2:] + str(bin(value_of_pt_1))
+    # serial_object.write(text_to_send)
+
+    previous_values_pt_2 = previous_values_pt_2[1:]
+    previous_values_pt_2.append(setvalue2.get())
+
+    create_plot2()
+
+
+
+create_plot1()
+create_plot2()
 
 window.title('Configuration of the SPI Potentiometer')
 window.geometry(f"{height}x{width}+{d_x}+{d_y}")
 window.minsize(width, height)
 window.maxsize(width, height)
-window.resizable(True, True)
 
 photo = tk.PhotoImage(file='setting.png')
 window.iconphoto(True, photo)
@@ -237,10 +337,10 @@ entry1.grid(row=2, column=3, padx=10)
 
 scale1_widg = tk.Scale(window, orient=tk.HORIZONTAL, variable=scale1)
 scale1_widg.grid(row=2, column=4, ipady=10)
-scale1_widg.config(state='disabled')
+scale1_widg.config(state=tk.DISABLED)
 scale1_widg.bind("<ButtonRelease-1>", set_values_scale_1)
 
-send_btn1 = tk.Button(window, text="Send")
+send_btn1 = tk.Button(window, text="Send", command=send_value_1)
 send_btn1.grid(row=2, column=5, padx=10)
 send_btn1.config(state="disabled")
 
@@ -285,7 +385,7 @@ scale2_widg.grid(row=4, column=4, ipady=10)
 scale2_widg.config(state="disabled")
 scale2_widg.bind("<ButtonRelease-1>", set_values_scale_2)
 
-send_btn2 = tk.Button(window, text="Send")
+send_btn2 = tk.Button(window, text="Send", command=lambda list1=previous_values_pt_2: send_value_2(list1))
 send_btn2.grid(row=4, column=5, padx=10)
 send_btn2.config(state="disabled")
 
@@ -297,10 +397,8 @@ val_2 = tk.Label(window, text=0)
 val_2.grid(row=4, column=7)
 val_2.config(state="disabled")
 
-# figure = plt.Figure(figsize=(6,5), dpi=100)
-# ax = figure.add_subplot(111)
-# chart_type = FigureCanvasTkAgg(figure, window)
-# chart_type.get_tk_widget().grid(row=8, column=0, columnspan=2, padx=10, pady=10)
+
+
 
 
 window.mainloop()
