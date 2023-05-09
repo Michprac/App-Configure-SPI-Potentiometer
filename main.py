cf80f4bd7645dee90d2a1ee880bfd1cb7381a8d4
Michprac <michal.poczta135@gmail.com>
2023-05-09 06:20:24 +0200

    Program v1.1

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ import serial
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-
-
 ports = list_ports.comports()
 serial_object = serial.Serial()
 all_ports_str = ['Port not selected']
@@ -35,22 +33,17 @@ def choose_pt():
 
 window = tk.Tk()
 
-
-
-
 height = 980
 width = 859
 d_x = 500
 d_y = 0
 Radio_pt = tk.IntVar()  # variable for the radiobutton choosing
 
-
 setvalue1 = tk.StringVar()  # actual value for the potentiometer 1
 scale1 = tk.IntVar()
 
 setvalue2 = tk.StringVar()  # actual value for the potentiometer 2
 scale2 = tk.IntVar()
-
 
 pt1_active = tk.IntVar()
 pt1_active.set(0)
@@ -61,24 +54,23 @@ ValuesForPotentiometer = {'Choose value': 'Choose value',
                           '40 000': 40000,
                           '50 000': 50000}
 
-previous_values_pt_1 = [0]*10
-actual_value_number_1 = list(range(1,11))
-previous_values_pt_2 = [0]*10
-actual_value_number_2 = list(range(1,11))
+previous_values_pt_1 = [0] * 10
+actual_value_number_1 = list(range(1, 11))
+previous_values_pt_2 = [0] * 10
+actual_value_number_2 = list(range(1, 11))
 
-#Creating a plot of previous values
+
+# Creating a plot of previous values
 def create_plot1():
-
     print(previous_values_pt_1)
     print(actual_value_number_1)
 
-    fig = plt.figure(figsize=(3,3), dpi=90)
-    plot1 = fig.add_subplot(1,1,1)
+    fig = plt.figure(figsize=(3, 3), dpi=90)
+    plot1 = fig.add_subplot(1, 1, 1)
     plot1.set_ylabel('Value of the potentiometer 1')
     plot1.set_xlabel('Previous value number')
     plot1.set_title('Graph for potentiometer 1')
     plot1.plot(actual_value_number_1, previous_values_pt_1, marker='o')
-
 
     plot_on_window = FigureCanvasTkAgg(fig, window)
     plot_on_window.draw()
@@ -86,15 +78,13 @@ def create_plot1():
 
 
 def create_plot2():
-
     print(previous_values_pt_2)
     print(actual_value_number_2)
 
-
     global fig, axis
 
-    fig = plt.figure(figsize=(3,3), dpi=90)
-    plot1 = fig.add_subplot(1,1,1)
+    fig = plt.figure(figsize=(3, 3), dpi=90)
+    plot1 = fig.add_subplot(1, 1, 1)
     plot1.set_ylabel('Value of the potentiometer 2')
     plot1.set_xlabel('Previous value number')
     plot1.set_title('Graph for potentiometer 2')
@@ -104,13 +94,6 @@ def create_plot2():
     plot_on_window = FigureCanvasTkAgg(fig, window)
     plot_on_window.draw()
     plot_on_window.get_tk_widget().grid(row=6, column=0, columnspan=8, sticky='NWES', pady=5, ipady=50, padx=20)
-
-
-
-
-
-
-
 
 
 def unlock_widgets1():
@@ -168,6 +151,7 @@ def set_values_box_1(event):
 
     val_1['text'] = str(newval) + '[Ω]'
 
+
 def set_values_box_2(event):
     if box2.get() == 'Choose value':
         newval2 = '0'
@@ -193,7 +177,7 @@ def set_values_entry_1(event):
         return
 
     if newval >= 0 and newval <= 50000:
-        setvalue1.set(newval)
+        setvalue1.set(str(newval))
     else:
         return
 
@@ -213,7 +197,7 @@ def set_values_entry_2(event):
         return
 
     if newval >= 0 and newval <= 50000:
-        setvalue2.set(newval)
+        setvalue2.set(str(newval))
     else:
         return
 
@@ -223,16 +207,17 @@ def set_values_entry_2(event):
 
     val_2['text'] = str(newval) + '[Ω]'
 
+
 def set_values_scale_1(event):
     if scale1_widg["state"] == "disabled":
         return
     newval = int(round(scale1.get() / 100 * max(list(ValuesForPotentiometer.values())[1:])))
-    setvalue1.set(newval)
+    setvalue1.set(str(newval))
 
     box1.set(list(ValuesForPotentiometer.keys())[0])
 
     entry1.delete(0, tk.END)
-    entry1.insert(0, newval)
+    entry1.insert(0, str(newval))
 
     val_1['text'] = str(newval) + '[Ω]'
 
@@ -242,15 +227,14 @@ def set_values_scale_2(event):
         return
 
     newval = int(round(scale2.get() / 100 * max(list(ValuesForPotentiometer.values())[1:])))
-    setvalue2.set(newval)
+    setvalue2.set(str(newval))
 
     box2.set(list(ValuesForPotentiometer.keys())[0])
 
     entry2.delete(0, tk.END)
-    entry2.insert(0, newval)
+    entry2.insert(0, str(newval))
 
     val_2['text'] = str(newval) + '[Ω]'
-
 
 
 def send_value_1():
@@ -262,26 +246,25 @@ def send_value_1():
 
     global previous_values_pt_1, actual_value_number_1
 
-
-
-    # configure_byte = 0b00010001
-    # value_of_pt_hex_1 = int(setvalue1.get()) * 255 // max(list(ValuesForPotentiometer.values())[1:])
-    # text_to_send = str(configure_byte)[2:] + str(bin(value_of_pt_1))
-    # serial_object.write(text_to_send)
-
+    configure_byte = 0b00010001
+    value_of_pt_hex_1 = int(setvalue1.get()) * 255 // max(list(ValuesForPotentiometer.values())[1:])
+    text_to_send = str(configure_byte)[2:] + str(bin(value_of_pt_hex_1))
+    try:
+        serial_object.write(text_to_send)
+    except serial.serialutil.PortNotOpenError:
+        showerror(title='Error', message='Connection error: Select a port')
+        return
 
     previous_values_pt_1 = previous_values_pt_1[1:]
     previous_values_pt_1.append(int(setvalue1.get()))
 
     actual_value_number_1 = actual_value_number_1[1:]
-    actual_value_number_1.append(actual_value_number_1[-1]+1)
+    actual_value_number_1.append(actual_value_number_1[-1] + 1)
 
     create_plot1()
 
 
-
 def send_value_2(list1):
-
     try:
         int(setvalue2.get())
     except ValueError:
@@ -290,20 +273,24 @@ def send_value_2(list1):
 
     global previous_values_pt_2, actual_value_number_2
 
+    configure_byte = 0b00010010
+    value_of_pt_1 = int(setvalue2.get()) * 255 // max(list(ValuesForPotentiometer.values())[1:])
+    text_to_send = str(configure_byte)[2:] + str(bin(value_of_pt_1))
+    serial_object.write(text_to_send)
 
-    # configure_byte = 0b00010010
-    # value_of_pt_1 = int(setvalue2.get()) * 255 // max(list(ValuesForPotentiometer.values())[1:])
-    # text_to_send = str(configure_byte)[2:] + str(bin(value_of_pt_1))
-    # serial_object.write(text_to_send)
+    try:
+        serial_object.write(text_to_send)
+    except serial.serialutil.PortNotOpenError:
+        showerror(title='Error', message='ConnectionError: Select a port')
+        return
 
     previous_values_pt_2 = previous_values_pt_2[1:]
     previous_values_pt_2.append(int(setvalue2.get()))
 
     actual_value_number_2 = actual_value_number_2[1:]
-    actual_value_number_2.append(actual_value_number_2[-1]+1)
+    actual_value_number_2.append(actual_value_number_2[-1] + 1)
 
     create_plot2()
-
 
 
 create_plot1()
@@ -410,9 +397,5 @@ actual_val_2.config(state="disabled")
 val_2 = tk.Label(window, text=0)
 val_2.grid(row=4, column=7)
 val_2.config(state="disabled")
-
-
-
-
 
 window.mainloop()
